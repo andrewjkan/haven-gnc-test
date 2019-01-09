f518e697cca435a29d81af22beddc05ed42ce129
Andrew <andrewjkan@gmail.com>
2019-01-09 12:24:11 -0800

    Added SASS support and content.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
-import './App.css';
+import logoGNC from './logo-gnc.svg';
+import logoSweeps from './logo-sweeps.svg';
+import logoLiveNation from './logo-live-nation.png';
+// import background from './bg.jpg';
+import './App.scss';
 
 class App extends Component {
   render() {
@@ -9,19 +12,33 @@ class App extends Component {
 
         <div className="container">
           <div className="header row">
-            <div className="col-md-10">Logo Area</div>
+            <div className="col-md-10">
+              <img src={logoGNC} className="logo-gnc" alt="GNC - Live Well" />
+              <img src={logoLiveNation} className="logo-live-nation" alt="Live Nation - Concerts" />
+            </div>
             <div className="col-md-2">Share Icons</div>
           </div> {/*END of header*/}
           <div className="main-content row">
             <div className="instructions col-lg-6">
               <div className="row">
-                <div className="col">GNC logo</div>
+                <div className="col">
+                  <img src={logoSweeps} className="logo-sweeps" alt="GNC - Ticket to Rock" />
+                </div>
               </div>
               <div className="row">
-                <div className="col">Ribbon</div>
+                <div className="col">
+                  <h2>Enter for a chance to win a Rock Flight® to a Live Nation® concert of your choice!+</h2>
+                </div>
               </div>
               <div className="row">
-                <div className="col">Instructions Copy</div>
+                <div className="col">
+                  <h3>1 Grand Prize winner will receive:</h3>
+                  <ul>
+                    <li>2 tickets to a Live Nation concert in the U.S.+</li>
+                    <li>Airfare & hotel accommodations for 2</li>
+                    <li>$200 cash gift card</li>
+                  </ul>
+                </div>
               </div>
             </div> {/*END of instructions*/}
             <div className="contact-form col-lg-6">
@@ -73,7 +90,22 @@ class App extends Component {
           </div> {/*END of main-content*/}
 
           <div className="footer row">
-            <div className="col">Footer Copy</div>
+            <div className="col">
+              <p>
+                +Live Nation produced  promoted concert at an owned and operated venue in the U.S. Tickets are subject to program availability, venue capacity and age restrictions.
+              </p>
+              <p>
+                NO PURCHASE NECESSARY. Legal U.S. residents 18+, who reside within a 100 mile radius of New York City, NY, Chicago, IL,  Houston, TX, Cleveland, OH or Atlanta, GA. Void where prohibited by law.  Begins at 12:00 PM PT on 2/9/15 and ends at 11:59 PM PT on 3/31/15. See details and <a href="#" target="_self">Official Rules</a>. To enter without a GNC Game Code, <a href="#" target="_self">Click here</a> for alternative method of entry.
+              </p>
+              <p>
+                The Sponsor of the Sweepstakes is GNC® Holdings Inc. The Administrator of the Sweepstakes is Live Nation Marketing, Inc., 7060 Hollywood Blvd., Los Angeles, CA 90028. The Sweepstakes and all accompanying materials are ©2015 Live Nation Worldwide, Inc. Live Nation and Rock Flight are registered trademarks of Live Nation Worldwide, Inc.
+              </p>
+              <ul>
+                <li><a href="#" target="_self">Click Here</a> for your nearest GNC location.</li>
+                <li><a href="#" target="_self">Live Nation Privacy Policy</a></li>
+                <li><a href="#" target="_self">GNC Privacy Policy</a></li>
+              </ul>
+            </div>
           </div> {/*END of footer*/}
 
         </div> {/*END of container*/}
