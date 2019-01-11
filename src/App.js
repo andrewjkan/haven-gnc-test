b45b2d6b0934112d42d7db9ce6ca3897acbbaab1
Andrew <andrewjkan@gmail.com>
2019-01-11 15:15:25 -0800

    Clean up.

--- src/App.js
+++ src/App.js
@@ -148,24 +148,7 @@ class App extends Component {
               </ul>
             </div>
           </div> {/*END of footer*/}
-
         </div> {/*END of container*/}
-
-        {/*<header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>*/}
-        
       </div>
     );
   }
