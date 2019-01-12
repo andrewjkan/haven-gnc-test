c32253e0dfe17d6d48f79e6f3cc1d920eb71aa82
Andrew <andrewjkan@gmail.com>
2019-01-11 16:53:44 -0800

    Added overlay for official rules.

--- src/App.js
+++ src/App.js
@@ -97,7 +97,7 @@ class App extends Component {
                   <div className="row">
                     <div className="col">
                       <input type="checkbox" id="checkbox-1" name="checkbox-1" value="agree"></input>
-                      <label htmlFor="checkbox-1">I agree to the <a href="#" target="_self">Official Rules</a></label>
+                      <label htmlFor="checkbox-1">I agree to the <a href="#" className="rules" target="_self">Official Rules</a></label>
                     </div>
                   </div>
                   <div className="row">
@@ -136,7 +136,7 @@ class App extends Component {
                 +Live Nation produced  promoted concert at an owned and operated venue in the U.S. Tickets are subject to program availability, venue capacity and age restrictions.
               </p>
               <p>
-                NO PURCHASE NECESSARY. Legal U.S. residents 18+, who reside within a 100 mile radius of New York City, NY, Chicago, IL,  Houston, TX, Cleveland, OH or Atlanta, GA. Void where prohibited by law.  Begins at 12:00 PM PT on 2/9/15 and ends at 11:59 PM PT on 3/31/15. See details and <a href="#" target="_self">Official Rules</a>. To enter without a GNC Game Code, <a href="#" target="_self">Click here</a> for alternative method of entry.
+                NO PURCHASE NECESSARY. Legal U.S. residents 18+, who reside within a 100 mile radius of New York City, NY, Chicago, IL,  Houston, TX, Cleveland, OH or Atlanta, GA. Void where prohibited by law.  Begins at 12:00 PM PT on 2/9/15 and ends at 11:59 PM PT on 3/31/15. See details and <a href="#" className="rules" target="_self">Official Rules</a>. To enter without a GNC Game Code, <a href="#" target="_self">Click here</a> for alternative method of entry.
               </p>
               <p>
                 The Sponsor of the Sweepstakes is GNC® Holdings Inc. The Administrator of the Sweepstakes is Live Nation Marketing, Inc., 7060 Hollywood Blvd., Los Angeles, CA 90028. The Sweepstakes and all accompanying materials are ©2015 Live Nation Worldwide, Inc. Live Nation and Rock Flight are registered trademarks of Live Nation Worldwide, Inc.
